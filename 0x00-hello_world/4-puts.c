b9012f827562d6f08c1702521dec44a168408ff8
SE-Mike <msquare1331988@gmail.com>
2023-09-07 17:13:14 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,13 @@
-
-
 #include <stdio.h>
+
 /**
  * main - main is an important function in C
+ *
  * Return: display 0
  */
 
 int main(void)
 {
          puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+	 return (0);
 }
