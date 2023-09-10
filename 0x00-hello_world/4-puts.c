945ed4b66dc8aaf8173a04524c9964a3cd455bf4
SE-Mike <msquare1331988@gmail.com>
2023-09-10 14:38:28 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
 
 /**
- * main - main is an important function in C
+ * main - Main is an important function in C
  *
- * Return: always 0
+ * Return:Always 0 when successful
  */
 
 int main(void)
 {
-	 puts("\"Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle,\n");
 	 return (0);
+	 /* returns 0 when successful otherwise 1 */
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,2 @@
-#include <stdio.h>
 
-/**
- * main - this function prints a strings 
- *
- * return: always 0
- */
-
-int main(void)
-{
-	printf("with proper grammar, but the outcome is a piece of art,");
-	return (0);
-}
 
