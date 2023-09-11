59edb8bad84520d3dca7fba70cdb8a02ee0d5bbc
SE-Mike <msquare1331988@gmail.com>
2023-09-11 12:30:23 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,12 @@
 /**
  * main - Main is an important function in C
  *
- * Return:Always 0 when successful
+ * Return:Always 0(success)
  */
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	 return (0);
+	puts("\"Programming is like building a multilingual puzzle,\n");
+	return (0);
 	 /* returns 0 when successful otherwise 1 */
 }
