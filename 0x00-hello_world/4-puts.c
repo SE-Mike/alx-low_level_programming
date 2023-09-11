fa55df3f689c013770bb7b32270098d54516e9a9
SE-Mike <msquare1331988@gmail.com>
2023-09-11 01:05:09 -0700

    printf.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle,\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	 return (0);
 	 /* returns 0 when successful otherwise 1 */
 }
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,14 @@
+#include <stdio.h>
 
+/**
+ * main - main function prints the string of word
+ *
+ * Return:Always 0 (success)
+ */
 
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+	/* return 0 when successful otherwise 1*/
+}
